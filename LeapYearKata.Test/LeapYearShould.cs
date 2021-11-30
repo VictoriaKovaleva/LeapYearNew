915e7773373e47b317bda9f588e8c4e4c8467f6e
vkovaleva <vkovaleva@voxelgroup.net>
2021-11-30 13:26:54 +0100

    test: 400 return Leap Year, deleted  code to start over again

--- LeapYearKata.Test/LeapYearShould.cs
+++ LeapYearKata.Test/LeapYearShould.cs
@@ -7,10 +7,7 @@ namespace LeapYearKata.Test
     public class LeapYearShould
     {
         [Theory]
-        [InlineData(2000, "Leap Year!")]
-        [InlineData(2004, "Leap Year!")]
-        [InlineData(2005, "Not Leap Year!")]
-        [InlineData(1350, "Not Leap Year!")]
+        [InlineData(400, "Leap Year!")]
         public void CheckIsLeapOrNotLeapYear(int year, string expected)
         {
             string actual = LeapYearChecker.CheckIsLeapYear(year);
--- LeapYearKata/LeapYearChecker.cs
+++ LeapYearKata/LeapYearChecker.cs
@@ -4,12 +4,7 @@
     {
         public static string CheckIsLeapYear(int year)
         {
-            if (year % 400 == 0 || year % 4 == 0)
-                return "Leap Year!";
-            if (year % 100 == 0 && year % 400 != 0)
-                return "Not Leap Year!";
-
-            return "Not Leap Year!";
+           return "";
         }
     }
 }
