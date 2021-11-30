b8eae374b6f33f2fcca6dd1a0152d2cdeaa5c1ae
vkovaleva <vkovaleva@voxelgroup.net>
2021-11-30 13:35:30 +0100

    test: 1300 return Not Leap Year

--- LeapYearKata.Test/LeapYearShould.cs
+++ LeapYearKata.Test/LeapYearShould.cs
@@ -10,6 +10,7 @@ namespace LeapYearKata.Test
         [InlineData(400, "Leap Year!")]
         [InlineData(800, "Leap Year!")]
         [InlineData(4000, "Leap Year!")]
+        [InlineData(1300, "Not Leap Year!")]
         public void CheckIsLeapOrNotLeapYear(int year, string expected)
         {
             string actual = LeapYearChecker.CheckIsLeapYear(year);
